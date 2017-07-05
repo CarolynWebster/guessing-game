d676025ef297371823980c6900af5310ccaf9361
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-05 15:48:00 -0700

    Scope fix, syntax fix, delete variables

--- game.py
+++ game.py
@@ -5,27 +5,23 @@ import random
 
 print "Hey there! Let's play a game"
 userName = raw_input("What's your name? ")
-print userName + "I'm thinking of a number between 1 and 100. \nTry to guess my number. "
+print userName + " I'm thinking of a number between 1 and 100. \nTry to guess my number. "
 
 secretNumber = random.randint(1, 100)
-tooHigh = 101
-tooLow = 0
 guess = None
 totalG = 0
 
 
-def checkGuess(totalG):
+def checkGuess():
     guess = int(raw_input("Your guess? "))
-    totalG = totalG + 1
     if guess > secretNumber:
         print "Your guess is too high, try again."
-        #guess = int(raw_input("Your guess? "))
     elif guess < secretNumber:
         print "Your guess is too low, try again."
-        #guess = int(raw_input("Your guess? "))
     else:
         print "Well done, " + userName + "! You found my number in " + str(totalG) + " tries!"
-    return totalG
-print totalG
+    return guess
+
 while guess != secretNumber:
-    totalG = checkGuess(totalG)
+    totalG += 1
+    guess = checkGuess()
