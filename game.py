2b56faacff2a84c4b4f56ba5363474d0aa38e988
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-05 12:48:11 -0700

    Function clean up, add loop

--- game.py
+++ game.py
@@ -11,14 +11,19 @@ secretNumber = random.randint(1, 100)
 tooHigh = 101
 tooLow = 0
 guess = None
+totalGuesses = 0
 
 def checkGuess():
     guess = int(raw_input("Your guess? "))
+    totalGuesses += 1
     if guess > secretNumber:
         print "Your guess is too high, try again."
-        guess = int(raw_input("Your guess? "))
+        #guess = int(raw_input("Your guess? "))
     elif guess < secretNumber:
         print "Your guess is too low, try again."
-        guess = int(raw_input("Your guess? "))
+        #guess = int(raw_input("Your guess? "))
     else:
-        print "Well done, " + userName + "! You found my number in @@@@ tries"
+        print "Well done, " + userName + "! You found my number in " + str(totalGuesses) + " tries!"
+
+while guess != secretNumber:
+    checkGuess()
